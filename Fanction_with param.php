0fd80b1f801ea1238dc71e9125cf1e76f86d72c0
DET <rongdet218@gmail.com>
2025-07-28 00:21:16 +0700

    Add files via upload

---
+++ Fanction_with param.php
@@ -0,0 +1,13 @@
+<?php
+function gradeCalculate($score){
+        if($score > 90 || $score ==100){
+            return $grade = "A";
+        }
+    }elseif($score > 80 || $score ==90){
+            return $grade = "B";
+        }
+
+
+
+
+?>
